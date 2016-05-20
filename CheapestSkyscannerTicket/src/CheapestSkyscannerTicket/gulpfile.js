b4c092b8857a9cf01a6487b557499b223727d1da
Sergey <frodo89@nm.ru>
2016-05-20 21:56:54 +0300

    -add npm, gulp, bootstrap, angular, jquery -create gulp copy task -create project structure

---
+++ CheapestSkyscannerTicket/src/CheapestSkyscannerTicket/gulpfile.js
@@ -0,0 +1,22 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+
+var paths = { webroot: "./wwwroot/" };
+
+var itemsToCopy = {
+    './node_modules/angular/angular*.js': paths.webroot + 'lib/angular',
+    './node_modules/bootstrap/dist/css/bootstrap.css': paths.webroot + 'lib/bootstrap',
+    './node_modules/bootstrap/dist/js/bootstrap.js': paths.webroot + 'lib/bootstrap',
+    './node_modules/jquery/dist/jquery.js': paths.webroot + 'lib/jquery'
+};
+
+gulp.task('copy', function () {
+    for (var src in itemsToCopy) {
+        if (!itemsToCopy.hasOwnProperty(src)) continue;
+        gulp.src(src).pipe(gulp.dest(itemsToCopy[src]));
+    }
+});
