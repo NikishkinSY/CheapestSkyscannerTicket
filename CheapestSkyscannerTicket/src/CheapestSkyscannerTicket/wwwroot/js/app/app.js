b42c0a50611caf8e7843d33d717915415fc039a2
Sergey <frodo89@nm.ru>
2016-05-20 22:05:12 +0300

    -create server-side files -create structure angular app

---
+++ CheapestSkyscannerTicket/src/CheapestSkyscannerTicket/wwwroot/js/app/app.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app', [
+        // Angular modules 
+        // Custom modules 
+        'app.core',
+        // 3rd Party Modules
+    ]);
+})();
